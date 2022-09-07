02391e2ac0b3d2d36b4362bf1ec20f4a55e39ac1
Jartto <315764765@qq.com>
2022-09-07 22:51:29 +0800

    feat: complete guess_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,20 +1,40 @@
 // 引入标准库输入/输出
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
-    let secret_numer = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_numer);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    // println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // 声明可变变量，并绑定一个新的空白字符串
-    let mut guess = String::new();
+        // 声明可变变量，并绑定一个新的空白字符串
+        let mut guess = String::new();
 
-    // 处理终端中的标准输入
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        // 处理终端中的标准输入
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // 通配符，匹配所有可能的 Err 值
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
