1a361bf065179c147be41509d2e029350fab4256
Jartto <315764765@qq.com>
2022-10-16 22:09:54 +0800

    feat: update rdm

---
+++ temperature_convert/target/debug/deps/temperature_convert-72d2f323d6649e0c.d
@@ -0,0 +1,5 @@
+/Users/jartto/Documents/Project/my-rust-practice/temperature_convert/target/debug/deps/temperature_convert-72d2f323d6649e0c.rmeta: src/main.rs
+
+/Users/jartto/Documents/Project/my-rust-practice/temperature_convert/target/debug/deps/temperature_convert-72d2f323d6649e0c.d: src/main.rs
+
+src/main.rs:
---
+++ temperature_convert/target/debug/deps/temperature_convert-afc6d6a51c5c4e24.d
@@ -0,0 +1,5 @@
+/Users/jartto/Documents/Project/my-rust-practice/temperature_convert/target/debug/deps/temperature_convert-afc6d6a51c5c4e24.rmeta: src/main.rs
+
+/Users/jartto/Documents/Project/my-rust-practice/temperature_convert/target/debug/deps/temperature_convert-afc6d6a51c5c4e24.d: src/main.rs
+
+src/main.rs:
