140215de3310e0149e9039d76cec86a74217a235
Jartto <315764765@qq.com>
2022-10-18 09:26:10 +0800

    feat: update temp_covert2

--- temperature_convert_2/src/main.rs
+++ temperature_convert_2/src/main.rs
@@ -1,4 +1,24 @@
 // 参考：https://blog.csdn.net/qq_36393978/article/details/125805264
+use std::io;
+
+enum Type {
+    Fahrenheit,
+    Celsius,
+    None,
+}
+
+struct Temp {
+    number: f64,
+    temp_type: Type,
+}
+
+fn get_temp() -> Temp {
+    let mut input = String::new();
+    let mut temp_input: Temp = Temp {
+        number: 0.0,
+        temp_type: Type::None,
+    };
+}
 
 fn main() {
     println!("Hello, world!");
---
+++ temperature_convert_2/target/debug/deps/temperature_convert_2-a0b1c03557d76d8d.d
@@ -0,0 +1,5 @@
+/Users/jartto/Documents/Project/my-rust-practice/temperature_convert_2/target/debug/deps/temperature_convert_2-a0b1c03557d76d8d.rmeta: src/main.rs
+
+/Users/jartto/Documents/Project/my-rust-practice/temperature_convert_2/target/debug/deps/temperature_convert_2-a0b1c03557d76d8d.d: src/main.rs
+
+src/main.rs:
