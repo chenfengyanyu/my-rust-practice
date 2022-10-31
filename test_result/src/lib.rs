f085e4f4497b13b19e64253c7dd619b5d949ec25
Jartto <315764765@qq.com>
2022-10-31 08:25:36 +0800

    feat: update test_result

---
+++ test_result/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if add(2, 2) == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus does not equal four"))
+        }
+    }
+}
---
+++ test_result/target/debug/deps/test_result-67d2e62d7f63622b.d
@@ -0,0 +1,5 @@
+/Users/jartto/Documents/Project/my-rust-practice/test_result/target/debug/deps/test_result-67d2e62d7f63622b: src/lib.rs
+
+/Users/jartto/Documents/Project/my-rust-practice/test_result/target/debug/deps/test_result-67d2e62d7f63622b.d: src/lib.rs
+
+src/lib.rs:
